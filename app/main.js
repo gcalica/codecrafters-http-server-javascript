0e32a69abb185660fda9a0a9a8e801bd3880509e
Gian Calica <calicagian7@gmail.com>
2023-10-19 19:22:13 -0700

    stage: Get a file

--- app/main.js
+++ app/main.js
@@ -86,7 +86,7 @@ function processGetHttpRequest(socket, headers, path, protocol) {
   } else if (apiAction === "files") {
     const filename = path.substring("/files/".length);
 
-    fs.readFileSync(`${directory}${filename}`, (err, filedata) => {
+    fs.readFile(`${directory}${filename}`, (err, filedata) => {
       if (err) {
         const response = new ResponseBuilder()
           .notFound(protocol)
