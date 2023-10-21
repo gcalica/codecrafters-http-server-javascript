6ffbec7f355c2a5ef7d491c7fd551e981ec9f972
Gian Calica <calicagian7@gmail.com>
2023-10-21 11:28:17 -0700

    stage: Post a file

--- app/main.js
+++ app/main.js
@@ -22,6 +22,7 @@ const HTTP_CODE = {
 };
 const HTTP_VERBS = {
   GET: "GET",
+  POST: "POST",
 };
 
 const server = net.createServer((socket) => {
@@ -37,6 +38,8 @@ const server = net.createServer((socket) => {
       case HTTP_VERBS.GET:
         processGetHttpRequest(socket, headers, path, protocol);
         break;
+      case HTTP_VERBS.POST:
+        processPostHttpRequest(socket, headers, path, protocol);
       default:
         break;
     }
@@ -52,6 +55,7 @@ function parseHttpRequest(data) {
   const [startLine, ...headers] = decodedToString.split("\r\n");
   const [method, path, protocol] = startLine.split(" ");
 
+  console.log(decodedToString)
   return { headers, method, path, protocol };
 }
 
@@ -84,8 +88,7 @@ function processGetHttpRequest(socket, headers, path, protocol) {
       .content(parsedUserAgent)
       .createResponse();
     socket.write(response);
-  } else if (apiAction === "files") {
-    const filename = path.substring("/files/".length);
+  } else if (apiAction === "files") { const filename = path.substring("/files/".length);
     const absPath = `${directory}${filename}`;
 
     if (fs.existsSync(absPath)) {
@@ -109,6 +112,19 @@ function processGetHttpRequest(socket, headers, path, protocol) {
     socket.write(response);
   }
   socket.end();
+}
+
+function processPostHttpRequest(socket, headers, path, protocol) {
+  const urlParams = path.substring(1).split("/");
+  const apiAction = urlParams[0];
+
+  if (apiAction === "files") {
+    const content = 
+
+  } else {
+    const response = new ResponseBuilder().notFound(protocol).createResponse();
+    socket.write(response);
+  }
 }
 
 class ResponseBuilder {
